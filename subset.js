2d93be5f4cd7c5a64beb02b53e662734d1d30be7
Shameer Salim <shameer.salim@aig.com>
2019-11-09 13:27:34 -0500

    subsets done

--- subset.js
+++ subset.js
@@ -1,14 +1,74 @@
-const input = [1, 2, 3];
 
-let subsets = [];
-generateSubsets(0, input, [], subsets);
-console.log('subsets', subsets);
+var subsets = function (nums) {
+    var sub = [
+        []
+    ];
 
-function generateSubsets(index, input, curr, subsets) {
-    subsets = subsets.concat(JSON.parse(JSON.stringify(curr)));
-    for (let i = index; i < input.length; i++) {
-        curr.push(input[i]);
-        generateSubsets(i + 1, input, curr, subsets);
-        curr = curr.slice(curr.length - 1);
+    combine([], nums.slice(0), sub);
+
+    return sub;
+};
+
+function combine(arr, nums, sub) {
+    while (nums.length) {
+        var data = nums[0];
+        var copy = arr.slice(0);
+        copy.push(data);
+        sub.push(copy);
+        nums.shift();
+        combine(copy, nums.slice(0), sub);
     }
 }
+
+
+/**
+ * [1,2,3]
+ * 1 -> [1]
+ * sub.push [1]
+ * 
+ * 1,2,3,4
+ * 
+ * 1
+ * 2
+ * 3
+ * 4
+ * 1,2
+ * 2,3
+ * 3,4
+ * 1,3
+ * 2,4
+ * 1,4
+ * 1,2,3
+ * 2,3,4
+ * 1,2,4
+ * 1,3,4
+ * 1,2,3,4
+ */
+
+subsets([1, 2, 3]);
+
+
+
+
+/*
+function generateSub(index, nums, curr, subsets) {
+    subsets.push(curr.slice(0));
+    for (let i = index; i < nums.length; i++) {
+        curr.push(nums[i]);
+        generateSub(i + 1, nums, curr, subsets);
+        curr.pop();
+    }
+}
+*/
+
+function gen(index, arr, curr, sub) {
+    sub.push(curr.slice(0));
+    for (let i = index; i < arr.length; i ++) {
+        curr.push(arr[i]);
+        gen(i+1, arr, curr, sub);
+        curr.pop();
+    }
+}
+sub1 = [];
+gen(0, [1,2,3], [], sub1);
+debugger;
